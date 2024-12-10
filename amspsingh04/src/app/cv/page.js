d7f39b3cd732b4b8399289299fef20d5507e1486
amspsingh04 <sp.singh.150604@gmail.com>
2024-12-11 02:15:23 +0530

    .

--- amspsingh04/src/app/cv/page.js
+++ amspsingh04/src/app/cv/page.js
@@ -1,6 +1,5 @@
 import styles from './page.module.css';
 import Header from '../header/page';
-import PdfViewer from './pdf.js';
 
 const CV = () => {
   return (
@@ -9,12 +8,18 @@ const CV = () => {
         <h1 className={styles.title}>My CV</h1>
       <p className={styles.description}>
         Below is a preview of my CV. You can also{' '}
-        <a href="/cv/cv.pdf" download className={styles.downloadLink}>
+        <a href="/CV.pdf" download className={styles.downloadLink}>
           download it here
         </a>.
       </p>
+      <br></br>
       <div className={styles.pdfViewer}>
-      <iframe src='/CV.pdf' />
+        <iframe 
+          src='/CV.pdf' 
+          width="100%" 
+          height="600px" 
+          title="CV" 
+        />
       </div>
     </div>
   );
