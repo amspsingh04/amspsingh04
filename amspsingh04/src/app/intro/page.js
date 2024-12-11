12313744a36460690e02ea5c714221b2cd274b10
amspsingh04 <sp.singh.150604@gmail.com>
2024-12-11 11:48:13 +0530

    .

--- amspsingh04/src/app/intro/page.js
+++ amspsingh04/src/app/intro/page.js
@@ -3,11 +3,22 @@ import styles from './page.module.css';
 const Intro = () => {
   return (
     <div className={styles.introContainer}>
+      {/* Rounded Image Section */}
+      <div className={styles.imageWrapper}>
+        <img
+          src="https://avatars.githubusercontent.com/u/93045361?v=4"
+          alt="Shourya Avatar"
+          className={styles.avatarImage}
+        />
+      </div>
+
+      {/* Main Text Section */}
       <div className={styles.centerTextSection}>
         <h2 className={styles.subtitle}>CS Junior at VITC</h2>
         <h1 className={styles.mainTitle}>Hi, I am Shourya Pratap Singh</h1>
         <h2 className={styles.subtitle}>From India</h2>
       </div>
+
       <div className={styles.columnsContainer}>
         <div className={styles.leftColumn}>
           <p>
