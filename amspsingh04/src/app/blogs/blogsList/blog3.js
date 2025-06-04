ea20be3151d615942f55ce0cf362d03188bbb800
amspsingh04 <sp.singh.150604@gmail.com>
2025-06-04 15:22:40 +0530

    ?

--- amspsingh04/src/app/blogs/blogsList/blog3.js
+++ amspsingh04/src/app/blogs/blogsList/blog3.js
@@ -3,14 +3,15 @@ const blog3 = {
   title: "Biomedical Image Segmentation",
   date: "2025-06-04",
   author: "Shourya Pratap Singh",
-  content: `
-    <iframe 
-      src="../public/Review_Biomedical_Image_Segmentation.pdf" 
-      width="100%" 
-      height="600px" 
-      style="border: none;">
-    </iframe>
-  `
+  content: 
+    <div className={styles.pdfViewer}>
+            <iframe 
+              src='/ShouryaPratapSingh_CV_research.pdf' 
+              width="100%" 
+              height="1000px" 
+              title="CV" 
+            />
+    </div>
 };
 
 export default blog3;
