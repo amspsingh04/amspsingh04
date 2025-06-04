be6c76cc3e39dbfeb5a751a95e14566f63e5c633
amspsingh04 <sp.singh.150604@gmail.com>
2025-06-04 15:38:03 +0530

    bruhx4

--- amspsingh04/src/app/blogs/blogsList/blog3.js
+++ amspsingh04/src/app/blogs/blogsList/blog3.js
@@ -7,7 +7,7 @@ const blog3 = {
     <iframe 
               src='/Review_Biomedical_Image_Segmentation.pdf' 
               width="100%" 
-              height="250px" 
+              height="750px" 
               title="CV" 
             />
 };
