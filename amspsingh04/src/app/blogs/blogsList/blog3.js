4fca00702a0aaf6e76e9677e44ae7bf976608b19
amspsingh04 <sp.singh.150604@gmail.com>
2025-06-04 15:13:29 +0530

    Paper

---
+++ amspsingh04/src/app/blogs/blogsList/blog3.js
@@ -0,0 +1,16 @@
+const blog3 = {
+  id: 3,
+  title: "My Research Paper",
+  date: "2025-06-04",
+  author: "Shourya Pratap Singh",
+  content: `
+    <iframe 
+      src="../../../../public/Review_Biomedical_Image_Segmentation.pdf" 
+      width="100%" 
+      height="600px" 
+      style="border: none;">
+    </iframe>
+  `
+};
+
+export default blog3;
--- amspsingh04/src/app/blogs/blogsList/index.js
+++ amspsingh04/src/app/blogs/blogsList/index.js
@@ -1,6 +1,7 @@
 import blog1 from "./blog1";
 import blog2 from "./blog2";
+import blog3 from "./blog3";
 
-const blogs = [blog1, blog2];
+const blogs = [blog1, blog2, blogs3];
 
 export default blogs;
