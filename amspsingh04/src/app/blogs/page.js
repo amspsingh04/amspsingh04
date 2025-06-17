abfc95c73af543a169499cf5ed93d8db31f6d0b1
amspsingh04 <sp.singh.150604@gmail.com>
2025-06-17 16:16:56 +0530

    ty

--- amspsingh04/src/app/blogs/page.js
+++ amspsingh04/src/app/blogs/page.js
@@ -4,7 +4,7 @@ import blogs from './blogsData';
 export default function BlogsLanding() {
   return (
     <div style={{ padding: '2rem', background: '#111', minHeight: '100vh', color: 'white' }}>
-      <h1 style={{ textAlign: 'center' }}>📚 My Blogs</h1>
+      <h1 style={{ textAlign: 'center' }}> My Blogs</h1>
       <div style={{ display: 'flex', flexWrap: 'wrap', gap: '1.5rem', justifyContent: 'center' }}>
         {blogs.map((blog) => (
           <Link key={blog.id} href={`/blogs/${blog.slug}`} style={{ textDecoration: 'none', color: 'inherit' }}>
