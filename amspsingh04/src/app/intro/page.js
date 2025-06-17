e32b232907cb906b4983bc877306428ba6c87824
amspsingh04 <sp.singh.150604@gmail.com>
2025-06-17 16:28:37 +0530

    .

--- amspsingh04/src/app/intro/page.js
+++ amspsingh04/src/app/intro/page.js
@@ -14,7 +14,7 @@ const Intro = () => {
 
       {/* Main Text Section */}
       <div className={styles.centerTextSection}>
-        <h2 className={styles.subtitle}>CS Junior at VITC</h2>
+        <h3 className={styles.subtitle}>CS Senior at VITC</h3>
         <h1 className={styles.mainTitle}>Hi, I am Shourya Pratap Singh</h1>
         <h2 className={styles.subtitle}>From India</h2>
       </div>
@@ -22,18 +22,17 @@ const Intro = () => {
       <div className={styles.columnsContainer}>
         <div className={styles.leftColumn}>
           <p>
-            I am a third-year student at VIT Chennai, studying Computer Science with Specialization in Cyber Physical Systems. I am interested in technologies such as Cloud Computing, AI/ML, Data Science, Computer Vision, and Digital Twins — technologies that can be used as comprehensive solutions for real-world problems.
+            I am a fourth-year student at VIT Chennai, studying Computer Science with Specialization in Cyber Physical Systems. I am interested in technologies such as Cloud Computing, AI/ML, Data Science, Computer Vision, and Digital Twins — technologies that can be used as comprehensive solutions for real-world problems.
           </p>
         </div>
         <div className={styles.rightColumn}>
           <p>
-            I have previously worked on projects like:
+            My research interests include:
           </p>
           <ul className={styles.projectList}>
-            <li>AMKR</li>
-            <li>TransHRNet++</li>
-            <li>ASR--</li>
-            <li>Nyaya-Sahaya</li>
+            <li>Image Processing and Computer Vision techniques (wrt Biomedical Applications)</li>
+            <li>Wearable AI Devices</li>
+            <li>Speech Recognition and Language Learning</li>
           </ul>
           <p>
             I would like to expand my knowledge and explore new areas for research and better solutions.
