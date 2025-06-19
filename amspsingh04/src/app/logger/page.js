8169479a2d19d7be3aaa2d37e662521d45d46cd3
amspsingh04 <sp.singh.150604@gmail.com>
2025-06-19 22:08:29 +0530

    fix logger page

--- amspsingh04/src/app/logger/page.js
+++ amspsingh04/src/app/logger/page.js
@@ -1,6 +1,6 @@
 'use client';
 import { useState, useEffect } from 'react';
-import styles from './logger.module.css';
+import styles from './page.module.css';
 
 export default function LoggerPage() {
   const [entries, setEntries] = useState([]);
